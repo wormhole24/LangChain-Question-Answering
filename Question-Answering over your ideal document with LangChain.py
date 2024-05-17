f3db84a7f15b6601261f44e8c7590384e296060e
Maryam Hashemi <smaryam.hashemit@gmail.com>
2024-05-17 22:29:28 +0200

    Update Question-Answering over your ideal document with LangChain.py

--- Question-Answering over your ideal document with LangChain.py
+++ Question-Answering over your ideal document with LangChain.py
@@ -1,41 +1,26 @@
-pip install --upgrade langchain openai -q
-pip install unstructured -q
-pip install unstructured[local-inference] -q
-pip install detectron2@git+https://github.com/facebookresearch/detectron2.git@v0.6#egg=detectron2 -q
-apt-get install poppler-utils
-pip install tiktoken -q
-pip install chromadb -q
-pip install pinecone-client -q
-
-
-
-
+!pip install langchain openai chromadb -q
+!pip install tiktoken unstructured configuration -q
+!pip install tiktoken -q
 
 import os
+import openai
+import langchain
+import tiktoken
+from langchain.llms import OpenAI  
+
 os.environ["OPENAI_API_KEY"] = " YOUR API KEY HERE "
 
-
-# Let's start with question and answering over a single document here( e.g. 'txt' file), in case there is no memory for the model, thus it would not refer to your earlier questions :
-
-
-
+# Let's start with question and answering over a single document here( e.g. 'txt' file), in case there is no memory for the model,
+#thus it would not refer to your earlier questions :
 
 from google.colab import drive
 drive.mount('/content/drive')
 
-
-
-
-
 location = "PATH TO YOUR FILE"
 address = os.path.join(location)
 
-
 # Loading the text file here, and tokenizing them by the 'TextLoader()' function
 # Here the llm is ”text-davinci-3″ by default
-
-
-
 
 from langchain.document_loaders import TextLoader
 loader = TextLoader(address)
@@ -43,26 +28,16 @@ loader = TextLoader(address)
 
 # Creating index out of the text
 
-
-
-
 from langchain.indexes import VectorstoreIndexCreator
 index = VectorstoreIndexCreator().from_loaders([loader])
 
 
 # Asking your questions
-
-
-
-
 query = "YOUR QUESTION HERE"
 index.query(query)
 
 
 # Now we try to add Question and Answering considering memory, also we elevate the llm model to "gpt-3.5-turbo" 
-
-
-
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -72,18 +47,10 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.chains.summarize import  load_summarize_chain
 
-
-
-
-
 directory = " Path to your file "
 Booklet = os.path.join(directory)
 loader = TextLoader(Booklet)
 docy = loader.load()
-
-
-
-
 
 llm = OpenAI(openai_api_key=" Your API key here ", model_name= " gpt-3.5-turbo " )
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -95,27 +62,14 @@ char_text_splitter = RecursiveCharacterTextSplitter(chunk_size =500, chunk_overl
 doc = char_text_splitter.split_documents(docy)
 print(len(doc))
 
-
 embeddings = OpenAIEmbeddings()
 #chroma vector database
 vectorstore = Chroma.from_documents(doc, embeddings)
 
-
-
-
-
 from langchain.memory import ConversationBufferMemory
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-
-
-
-
 qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), memory=memory)
-
-
-
-
 
 query = "Your question"
 result = qa({"question": query})
